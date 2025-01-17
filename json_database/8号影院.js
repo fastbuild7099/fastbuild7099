ca266a3311708502ebf62c94fbd883b9389762e0
fastbuild7099 <fastbuild7099@gmail.com>
2025-01-17 17:33:09 +0800

    s

--- json_database/8号影院.js
+++ json_database/8号影院.js
@@ -1,21 +1,39 @@
-muban.首图2.二级.desc = ';;;.data--span:eq(0)&&Text;.data--span:eq(1)&&Text';
-muban.首图2.二级.tabs = '.stui-pannel__head h3';
-var rule={
-	title:'8号影院',
-	模板:'首图2',
-	//host:'http://www.8hysw.com',
-	host:'http://www.bahaoyy.com',
-	// url:'/frim/fyclass-fypage.html',
-	url:'/frim/fyfilter.html',
-	filterable:1,//是否启用分类筛选,
-	filter_url:'{{fl.cateId}}-fypage',
-	filter: {"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作","v":"5"},{"n":"爱情","v":"6"},{"n":"科幻","v":"7"},{"n":"恐怖","v":"8"},{"n":"喜剧","v":"10"},{"n":"剧情","v":"12"},{"n":"在线直播","v":"28"}]}],"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"港台剧","v":"14"},{"n":"欧美剧","v":"15"},{"n":"日韩剧","v":"16"}]}]},
-	filter_def:{
-		1:{cateId:'movie'},
-		2:{cateId:'tvseries'},
-		3:{cateId:'anime'},
-	},
-	searchUrl:'/search.php?page=fypage&searchword=**&searchtype=',
-	class_parse: '.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-	搜索:muban.首图2.搜索2,
+Object.assign(muban.首图2.二级, {
+    tabs: '.stui-pannel__head h3',
+});
+var rule = {
+    模板: '首图2',
+    title: '8号影院',
+    host: 'http://www.bahaoys.com',
+    url: "/frim/fyfilter.html",
+    filterable: 1,//是否启用分类筛选,
+    filter_url: "{{fl.按分类}}-fypage",
+    tab_exclude: '本周热门|最近更新',
+    filter: "H4sIAAAAAAAAA6tWMlSyiq5Wyk6tVLJSetbT+bSj7fnG3Uo6SnmJualoQmWJOaWpYOV5QJmnXSue7J3zvLMdJAPkmyrV6kBknndsfNbcipAxQ8gsn/h0526EjDlc5lnjhGcN0xAyFnCZp9PmPO1cjpAxNEBIdS5HscjQSKk2tjZWR8mIbE/N3vtk13KguTATjREu3LH+af8GJCkThNSa5c/39SFJIcLi2fSlL+evRJIygzrRmHQngvWZkKWvFgCFYy+16QEAAA==",
+    filter_def: {
+    1: {
+      按分类: "1"
+    },
+    2: {
+      按分类: "2"
+    },
+    3: {
+      按分类: "3"
+    },
+    4: {
+      按分类: "4"
+    }
+  },
+    searchUrl: '/search.php?page=fypage&searchword=**   &searchtype=',
+    class_parse: ".stui-tou__menu li;a&&Text;a&&href;.*/(.*?).html",
+    lazy: $js.toString(() => {
+        let init_js = `Object.defineProperties(navigator, {platform: {get: () => 'iPhone'}});`;
+        input = {
+            parse: 1,
+            url: input,
+            js: '',
+            parse_extra: '&init_script=' + encodeURIComponent(base64Encode(init_js)),
+        }
+    }),
+    搜索: '.stui-vodlist__media&&li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
 }
